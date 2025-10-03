ca9518fd4eef3feb2d8a9aae1a0050925ac3589c
huyhao27 <haohuy2k5@gmail.com>
2025-10-03 09:08:01 +0700

    lvu

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,21 +10,29 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 movement;
+    
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
     float playerX, playerY;
     void Update()
     {
       
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        transform.localEulerAngles = new Vector3(0, movement.x < 0 ? 0 : 180, 0);
+        // movement.y = Input.GetAxisRaw("Vertical");
+        //flip
+        
         // playerX = Input.GetAxisRaw("Horizontal");
         // playerY = Input.GetAxisRaw("Vertical");
-        movement.Normalize();
+        // movement.Normalize();
         rb.MovePosition(rb.position + movement * (speed * Time.deltaTime));
+        animator.SetBool("isMove", movement.x!=0);
+        // animator.SetFloat("Vertical", movement.y!=);
+        
         
     }
 
