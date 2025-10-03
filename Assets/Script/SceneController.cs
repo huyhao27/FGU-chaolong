bbc1dc733e07b7e29c89549d820cc748778b312f
huyhao27 <haohuy2k5@gmail.com>
2025-10-03 08:48:39 +0700

    cacc

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        LoadScene(sceneName);
     }
 }
