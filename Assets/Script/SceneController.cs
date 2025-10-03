e21602a6a4484fe7f6fe0f701488274ba6084660
huyhao27 <haohuy2k5@gmail.com>
2025-10-03 08:56:17 +0700

    lolvu

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName);
     }
 }
